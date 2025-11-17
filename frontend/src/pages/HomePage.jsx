d54476a0366e92f2b7c887ec6368020cab08e783
Circe-melki <midory798464@gmail.com>
2025-11-18 00:35:56 +0100

    moving compenents

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,76 +1,22 @@
 
 
 
+import { Header } from '../components/Header';
 import './HomePage.css'
 
 
 export function HomePage() {
-  return (
-    <div className="bg-dark text-white min-vh-100 d-flex flex-column">
+  return (<><title>porfolio</title>
+  <div className="bg-dark text-white min-vh-100 d-flex flex-column">
       {/* Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-dark bg-black border-bottom border-secondary py-3">
-        <div className="container">
-          <a className="navbar-brand fw-bold" href="#">
-            Portfolio
-          </a>
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Testimonials
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Recent Work
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Get In Touch
-                </a>
-              </li>
-            </ul>
-
-            <div className="d-flex gap-3 social-icons">
-              <a href="#" className="text-white">
-                <i className="bi bi-linkedin"></i>
-              </a>
-              <a href="#" className="text-white">
-                <i className="bi bi-behance"></i>
-              </a>
-              <a href="#" className="text-white">
-                <i className="bi bi-twitter"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Header/>
 
       {/* Hero Section */}
       <section className="container text-center text-md-start d-flex flex-column flex-md-row align-items-center justify-content-center flex-grow-1 py-5">
         {/* Left: Profile Image */}
         <div className="me-md-5 mb-4 mb-md-0">
           <img
-            src="profile-photo.jpg"
+            src="profile.jpg"
             alt="Profile"
             className="rounded-circle hero-image shadow-lg"
             width="300"
@@ -91,6 +37,8 @@ export function HomePage() {
         </div>
       </section>
     </div>
+  </>
+    
   );
 }
 

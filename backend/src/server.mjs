122d50f6b69ade031ac4517021ffc8021d8df5b0
Circe-melki <midory798464@gmail.com>
2025-11-18 01:32:14 +0100

    adding routs to the server

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -1,10 +1,14 @@
 import express from 'express'
 
 import dotenv  from 'dotenv';
+import routers from './routers/index.mjs';
 dotenv.config()
 const app = express();
 
 const PORT= process.env.MAIN_PORT||3000;
+
+app.use(express.json());
+app.use('/api',routers);
 
 app.listen(PORT,()=>{
     console.log(`you are connected to port ${PORT}`)
